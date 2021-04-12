9504705c21af793015695f3b82f4eb51fd002955
wuhong <643023231@qq.com>
2021-04-12 18:05:06 +0800

    增加问题记录文件和读取md的插件

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,35 @@
 import * as React from "react"
 import { Link } from "gatsby"
-// import { StaticImage } from "gatsby-plugin-image"
-
+import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Sidebar from '../components/sidebar'
+// import { graphql, useStaticQuery } from "gatsby"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi guys</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Sidebar></Sidebar>
-    {/* <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    /> */}
-    <p>
-      <Link to="/page-2/">graphql is coming soon!</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+
+
+    return (
+      <Layout>
+      <Seo title="Home" />
+      <h1>Hi guys</h1>
+      <p>graphql show</p>
+      <p>Now go build something great.</p>
+      <Sidebar></Sidebar>
+      {/* <StaticImage
+        src="../images/gatsby-astronaut.png"
+        width={300}
+        quality={95}
+        formats={["AUTO", "WEBP", "AVIF"]}
+        alt="A Gatsby astronaut"
+        style={{ marginBottom: `1.45rem` }}
+      /> */}
+      <p>
+        <Link to="/page-2/">graphql is coming soon!</Link> <br />
+        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      </p>
+    </Layout>
+    )
+}
 
 export default IndexPage
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,30 @@
 import * as React from "react"
 import { Link } from "gatsby"
-
+// import { useStaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    <Seo title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = () => {
+  
+  // const data = useStaticQuery(graphql`
+  //   query SiteTitleQuery {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `)
+
+  return (
+    <Layout>
+      <Seo title="Page two" />
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2</p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+
+}
 
 export default SecondPage
